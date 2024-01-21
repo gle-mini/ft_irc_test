6165feb1c73eb33960f51d0ac4c992bec69a791f
Gurvan Le Minier <gleminier2@gmail.com>
2024-01-21 08:01:42 +0100

    delete irc.hpp in tests files

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,5 +1,4 @@
 #include "gtest/gtest.h"
-#include "irc.hpp"
 
 TEST(HelloTest, ReturnsZero) {
     EXPECT_EQ(hello(), 0);
--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,5 +1,4 @@
 #include "gtest/gtest.h"
-#include "irc.hpp"
 
 TEST(HelloTest, OutputsHelloWorld) {
     // Sauvegarder l'ancien buffer
